ae5c74c413b022a16e352e0ac7fcf96cdeb91c24
druke <druke@DESKTOP-NUQGNKC>
2023-01-29 18:03:40 +0100

    añadido ft_strlcpy añadido a libreria, no testado, no norminette

---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odruke <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 10:49:47 by odruke            #+#    #+#             */
+/*   Updated: 2022/11/17 09:17:12 by odruke           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size == 0)
+		return(ft_strlen(src));
+	while ((i < (size -1)) && (src[i] !='\0'))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return(ft_strlen(src));
+}
