1f1fa417f4aaa7dd5046f6a446e1294e2be936f6
druke <druke@DESKTOP-NUQGNKC>
2023-02-13 21:16:24 +0100

    terminado ft_strncmp, norminette pasada a falta de header42

--- libft/libft.h
+++ libft/libft.h
@@ -1,7 +1,9 @@
-# include <string.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdio.h>
+#ifndef LIBFT_H
+#define LIBFT_H
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 int ft_isalpha(int a);
 int ft_isdigit(int a);
 int ft_isascii(int a);
@@ -18,3 +20,6 @@ int     ft_tolower(int c);
 char    *ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+void *ft_memchr(const void *s, int c, size_t n);
+int ft_strncmp(const char *s1, const char *s2);
+#endif
