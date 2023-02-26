5b18659bda49f41dba1f31521545fa0520909199
druke <druke@DESKTOP-NUQGNKC>
2023-02-26 18:28:15 +0100

    ft_substr terminada, pasa francinette, pasa norminette

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: druke <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 22:01:33 by druke             #+#    #+#             */
-/*   Updated: 2023/02/21 20:31:52 by druke            ###   ########.fr       */
+/*   Updated: 2023/02/26 18:27:33 by druke            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*p;
 
-	if (!s) // cadena vacia
-		return (NULL); 
-	if ((size_t)start > ft_strlen(s)) // start fuera de la candena devuelve cadena vacia
+	if (!s)
+		return (NULL);
+	if ((size_t)start > ft_strlen(s))
 		return (s = (char *)ft_calloc(1, sizeof(char)));
-	if (ft_strlen(s + start) < len) //acotar len si queda fuera
-		len = ft_strlen(s + start); 
+	if (ft_strlen(s + start) < len)
+		len = ft_strlen(s + start);
 	p = (char *)malloc((len + 1) * sizeof(char));
 	if (!p)
 		return (NULL);
