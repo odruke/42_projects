4cecbb60df2d963c3925f7e371b26883351c3119
druke <druke@DESKTOP-NUQGNKC>
2023-02-26 18:09:14 +0100

    arreglado ft_atoi, norminette pasada, francinette pasada

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: druke <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/26 17:51:31 by druke             #+#    #+#             */
+/*   Updated: 2023/02/26 18:08:05 by druke            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 int	ft_atoi(const char *nptr)
@@ -5,7 +17,7 @@ int	ft_atoi(const char *nptr)
 	int	sign;
 	int	res;
 	int	i;
-	
+
 	sign = 1;
 	res = 0;
 	i = 0;
@@ -18,10 +30,7 @@ int	ft_atoi(const char *nptr)
 	while (ft_isdigit(nptr[i]))
 	{
 		if (ft_isdigit(nptr[i + 1]))
-		{
-			res = res * 10 + (nptr[i] - 48);
-			i++;
-		}
+			res = res * 10 + (nptr[i++] - 48);
 		else
 		{
 			res = res * 10 + (nptr[i] - 48);
