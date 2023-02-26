12531063f909328236c8690838624e38f84af6a2
druke <druke@DESKTOP-NUQGNKC>
2023-02-26 17:05:35 +0100

    corregido ft_memchr.c, pasa norminette y francinette

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: odruke <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 17:16:02 by odruke            #+#    #+#             */
-/*   Updated: 2023/02/18 17:16:06 by odruke           ###   ########.fr       */
+/*   Updated: 2023/02/26 17:02:40 by druke            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
 	unsigned char	*str;
 	unsigned char	fnd;
 
