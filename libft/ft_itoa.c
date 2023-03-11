5b328d22f073659a00a4bd9954e3dbe7edcb4963
odruke <118107592+odruke@users.noreply.github.com>
2023-03-11 19:27:35 +0100

    update ft_itoa.c

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -17,17 +17,39 @@ static long int	flen(unsigned long int n)
 	long int	len;
 
 	len = 0;
-	while (n > len)
-		len = len + (n )
+	while(n > 0)
+	{
+		n = (n / 10);
+		len++;
+	}
+	return (len);
 }
 
+/*static char	*str(char	*s, int n, unsigned long int len)
+{
+	
+}
+*/
 char	*ft_itoa(int n)
 {
 	char	*nc;
 	size_t	len;
+	unsigned long int temp;
 
+	temp = n;
 	len = flen(n);
-	if(!*nc = malloc((sizeof char) * len))
-		returnn (NULL);
-
+	if (n < 0)
+		len++;
+	if(!*nc = malloc((sizeof char) * (len + 1)))
+		return (NULL);
+	temp[len + 1] = '\0';
+	while (temp > 0)
+	{
+		nc[len] = temp % 10;
+		temp = temp / 10;
+		len--;
+	}
+	if (n < 0)
+		nc[len] = '-';
+	return (*nc);
 }
