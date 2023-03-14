55b710af68816bff640ed436e5c93a6a6d16d25a
druke <druke@DESKTOP-NUQGNKC>
2023-03-14 21:03:50 +0100

    terminado ft_putchar_fd, norminette ok, francinette ok, actalizados Makefile y libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: druke <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 20:38:41 by druke             #+#    #+#             */
-/*   Updated: 2023/03/13 21:26:19 by druke            ###   ########.fr       */
+/*   Updated: 2023/03/14 07:33:36 by druke            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,4 +47,5 @@ char 	*ft_itoa(int n);
 char	**ft_split(char const *s, char c);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
 #endif
