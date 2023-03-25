11ebc853b6c510cf619f8a4937b753b7df7a2d68
druke <druke@DESKTOP-NUQGNKC>
2023-03-25 20:12:58 +0100

    trabajando en split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: druke <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 12:38:25 by druke             #+#    #+#             */
-/*   Updated: 2023/03/19 17:18:55 by druke            ###   ########.fr       */
+/*   Updated: 2023/03/25 20:12:23 by druke            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,42 +23,54 @@ static int	nosubstr(const char *s, char del)
 	nosub = 0;
 	while (s[i])
 	{
-		if (s[i] == del)
-			check = 0;
 		if (s[i] != del && check == 0)
 		{
 			check = 1;
 			nosub++;
 		}
+		else if (s[i] == del)
+			check = 0;
 		i++;
 	}
 	return (nosub);
 }
 
-/*static char	*subs(char *s, char c)
+static char	*f_subs(char const *s, int start, unsigned long int len)
 {	
 	char	*sub;
 
-	while (s && *s != c)
-	{
-		sub = s;
-		sub++;
-		s++;	
-	}
+	sub = (char *)malloc((len + 1) * sizeof(char));
+	if (!sub)
+		return (NULL);
+	ft_strlcpy(sub, s + start, len + 1);
+	return (sub);
 }
-*/
-static int	f_subslen(const char *s, int start, char del)
+
+static int	f_subslen(const char *s, const int start, char del)
 {
 	int	size;
+	int	i;
 
+	i = start;
 	size = 0;
-	while (s[start] && s[start] != del)
+	while (s[i] && s[i] != del)
 	{
 		size++;
-		start++;
+		i++;
 	}
 	return (size);
 }
+
+static void	f_free(char **s, int i)
+{
+	while (i > 0)
+	{
+		free (s[i]);
+		i--;
+	}
+	free (s);
+}
+
 char **ft_split(char const *s, char c)
 {
 	char	**ns;
@@ -68,21 +80,31 @@ char **ft_split(char const *s, char c)
 	int	ii;
 
 	i = 0;
-	ii = -1;
-	nosub = nosubstr(s, c);
-	ns = (char **)malloc(sizeof(char *) * nosub);
-	if (!ns || !s)
+	ii = 0;
+	if (!s)
 		return (NULL);
-	while (ii++ < nosub)
+	if (!c)
+		return (NULL);
+	nosub = nosubstr(s, c);
+	if (nosub == 0)
+		return (NULL);
+	ns = (char **)malloc(sizeof(char *) * (nosub + 1));
+	if (!ns)
+		return (NULL);
+	while (ii != nosub)
 	{
 		while (s[i] == c)
 			i++;
 		subslen = f_subslen(s, i, c);
-		ns[ii] = ft_substr(s, i, subslen);
+		ns[ii] = f_subs(s, i, subslen);
 		if (!ns[ii])
+		{
+			f_free(ns, ii);
 			return (NULL);
-	//	free (ns[ii]);
+		}
 		i += subslen;
+		ii++;
 	}
+	ns[ii] = '\0';
 	return (ns);
 }
