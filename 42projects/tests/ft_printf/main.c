e94b50115433b68cd7e0c7b35193611f35b1005d
Oscar Druke <odruke@c1r8s3.42lausanne.ch>
2023-04-24 22:29:47 +0200

    modified main.c in test, improved, not finished

--- 42projects/tests/ft_printf/main.c
+++ 42projects/tests/ft_printf/main.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 //#include <ft_printf.h>
 #include "stdlib.h"
-
+#include "string.h"
 int	main()
 {
 	char	*str;
@@ -11,23 +11,25 @@ int	main()
 		printf("allocation failed");
 		return (0);
 	}
-	str = "something here";
+	strcpy(str, "something here");
 	printf("===========\n");
-	printf("this is char\n->%c<-\n",'a');
+	printf("this is char %%c\n->%c<-\n",'a');
 	printf("===========\n");
-	printf("this is string\n->%s<-\n",str);
+	printf("this is string %%s\n->%s<-\n",str);
 	printf("===========\n");
-	printf("this is void pointer\n->%p<-\n",(void *)str);
+	printf("this is void pointer %%p\n->%p<-\n",(void *)str);
 	printf("===========\n");
-	printf("this is decimal\n->%d<-\n",42);
+	printf("this is integer %%d\n->%d<-\n",42);
 	printf("===========\n");
-	printf("this is integer\n->%i<-\n",42);
+	printf("this is integer %%i\n->%i<-\n",42);
 	printf("===========\n");
-	printf("this is unsigned long\n->%u<-\n",42);
+	printf("this is octal %%i\n->%i<-\n",52);
 	printf("===========\n");
-	printf("this is hexadecimal\n->%x<-\n",42);
+	printf("this is unsigned long %%u\n->%u<-\n",42);
 	printf("===========\n");
-	printf("this is percentage symbol\n->%%<-\n", '%');
+	printf("this is hexadecimal %%x\n->%x<-\n",42);
+	printf("===========\n");
+	printf("this is percentage symbol %%\n->%%<-\n");
 
 
 	free(str);
